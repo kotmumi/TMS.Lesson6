134258bc279f0edc8cdfb7eb4f9cd21fb3c47a3d
Kotylo <kirillkotylo@gmail.com>
2025-03-11 18:48:23 +0300

    Create enum Name and enum Subjects

--- Lesson6_Kotylo.playground/Contents.swift
+++ Lesson6_Kotylo.playground/Contents.swift
@@ -7,3 +7,28 @@
 //Используйте кортеж для представления данных об одном студенте: (name: String, age: Int, grades: [String: Double], optionalSubjects: Set<String>)
 
 typealias Student = (name: String, age: Int, grades: [String: Double], optionalSubjects: Set<String>)
+
+// MARK: -2.  Создать массив студентов: Создайте массив, содержащий данные о нескольких студентах (минимум 5).
+enum Name: String, CaseIterable {
+    case Alex = "Ales"
+    case Ivan = "Ivan"
+    case Kirill = "Kirill"
+    case Leonid = "Leonid"
+    case Pavel = "Pavel"
+    case Serhii = "Serhii"
+    case Vladimer = "Vladimer"
+    case Yaroslav = "Yaroslav"
+    case Andrii = "Andrii"
+}
+
+enum Subjects: String, CaseIterable{
+    case math = "Math"
+    case english = "English"
+    case history = "History"
+    case physics = "Physics"
+    case chemistry = "Chemistry"
+    case biology = "Biology"
+    case computerScience = "Computer Science"
+    case french = "French"
+    case art = "Art"
+    }
