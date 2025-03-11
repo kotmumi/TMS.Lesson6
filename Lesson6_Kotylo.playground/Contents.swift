75d22dc461bbd12485cc1335182f4458cf8d175e
Kotylo <kirillkotylo@gmail.com>
2025-03-11 18:25:59 +0300

    Created a tuple with an alias Student

--- Lesson6_Kotylo.playground/Contents.swift
+++ Lesson6_Kotylo.playground/Contents.swift
@@ -1,3 +1,9 @@
-import UIKit
+// MARK: -1.  Хранить информацию о студентах: Для каждого студента необходимо хранить следующую информацию:
+//*   Имя (String)
+//*   Возраст (Int)
+//*   Оценки по предметам (Dictionary, где ключ - название предмета (String), значение - оценка (Int))
+//*   Набор (Set) посещенных факультативов (String)
 
-var greeting = "Hello, playground"
+//Используйте кортеж для представления данных об одном студенте: (name: String, age: Int, grades: [String: Double], optionalSubjects: Set<String>)
+
+typealias Student = (name: String, age: Int, grades: [String: Double], optionalSubjects: Set<String>)
